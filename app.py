9e613f241655ae5c964907ca9a54320258d97fde
myt111 <1229343081@qq.com>
2023-11-03 16:26:36 +0800

    Add static files

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-from flask import Flask
+from flask import Flask,render_template
 from markupsafe import escape
 from flask import url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return '<h1>Hello Flask ! </h1><img src="http://helloflask.com/totoro.gif">'
-
+# @app.route('/')
+# def hello():
+#     return '<h1>Hello Flask ! </h1><img src="http://helloflask.com/totoro.gif">'
 
 @app.route('/user/<name>')
 def user_page(name):
@@ -26,3 +25,21 @@ def test_url_for():
     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return url_for('user_page', name='peter')
+
+
+@app.route('/')
+def index():
+    name = 'Grey Li'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+    return render_template('index.html', name=name, movies=movies)
